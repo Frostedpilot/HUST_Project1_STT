74577dfd7469ead9a0545b6fc07c12094ba34942
Frostedpilot <Truong.VT225535@sis.hust.edu.vn>
2024-12-14 14:38:49 +0700

    add local model loading

--- app/utility.py
+++ app/utility.py
@@ -1,4 +1,12 @@
 import requests
+import warnings
+import torch
+from transformers import SpeechEncoderDecoderModel
+from transformers import AutoFeatureExtractor, AutoTokenizer, GenerationConfig
+from transformers import Wav2Vec2Model
+from faster_whisper import WhisperModel
+
+warnings.filterwarnings('ignore')
 
 def check_deepgram_api_key(api_key):
     url = 'https://api.deepgram.com/v1/auth/token'
@@ -42,6 +50,21 @@ def check_assemblyai_api_key(api_key):
 
 def load_whisper(model_size):
     print(f'Loading OpenAI Whisper: {model_size}')
+    model = WhisperModel(model_size, device='cuda', compute_type='float16')
+    return model
 
 def load_wav2vec(model_size):
-    print(f'Loading Facebook Wav2Vec: {model_size}')
+    if model_size == 'vietnamese':
+        print(f'Loading Facebook Wav2Vec: {model_size}')
+        model_path = 'nguyenvulebinh/wav2vec2-bartpho'
+        model = SpeechEncoderDecoderModel.from_pretrained(model_path).eval()
+        if torch.cuda.is_available():
+            model = model.cuda()
+        return model
+    elif model_size == 'english':
+        print(f'Loading Facebook Wav2Vec: {model_size}')
+        model_path = 'facebook/wav2vec2-base-960h'
+        model = Wav2Vec2Model.from_pretrained(model_path, torch_dtype=torch.float16)
+        if torch.cuda.is_available():
+            model = model.cuda()
+        return model
