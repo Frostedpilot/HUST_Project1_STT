f4b8ee3b32415d09790de6f4065126d30ff0954e
Frostedpilot <Truong.VT225535@sis.hust.edu.vn>
2024-12-14 13:08:55 +0700

    checkpoint

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QComboBox, QPushButton, QLineEdit, QWidget, QInputDialog
+import assemblyai as aai
+from deepgram import DeepgramClient, DeepgramApiKeyError
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QComboBox, QPushButton, QScrollArea, QWidget, QInputDialog
 from PyQt6.QtCore import Qt
 
 class MyMainWindow(QMainWindow):
@@ -14,11 +16,12 @@ class MyMainWindow(QMainWindow):
       self.setCentralWidget(main_widget)
 
       # Add widgets
-      self.add_model_combobox()
-      self.add_language_combobox()
+      self.add_model_combobox_part()
+      self.add_language_combobox_part()
       self.add_buttons()
+      self.add_transcript_output()
       
-   def add_model_combobox(self):
+   def add_model_combobox_part(self):
       main_layout = self.centralWidget().layout()
       layout = QHBoxLayout()
       layout.setAlignment(Qt.AlignmentFlag.AlignRight)
@@ -28,15 +31,12 @@ class MyMainWindow(QMainWindow):
       layout.addWidget(combo)
       main_layout.addLayout(layout)
 
-   def add_language_combobox(self):
+   def add_language_combobox_part(self):
       main_layout = self.centralWidget().layout()
       layout = QHBoxLayout()
       layout.setAlignment(Qt.AlignmentFlag.AlignRight)
       label = QLabel('Language')
-      combo = QComboBox()
-      combo.addItem('English')
-      combo.addItem('Vietnamese')
-      combo.addItem('Auto')
+      combo = LanguageComboBox()
       layout.addWidget(label)
       layout.addWidget(combo)
       main_layout.addLayout(layout)
@@ -48,6 +48,24 @@ class MyMainWindow(QMainWindow):
       button = QPushButton('Start')
       layout.addWidget(button)
       main_layout.addLayout(layout)
+
+   def add_transcript_output(self):
+      main_layout = self.centralWidget().layout()
+      scroll = QScrollArea()
+      widget = QWidget()
+      layout = QVBoxLayout()
+      widget.setLayout(layout)
+      
+      for i in range(100):
+         label = QLabel(f'Transcript {i}' + 'A' * 100)
+         layout.addWidget(label)
+      
+      scroll.setWidget(widget)
+      scroll.setWidgetResizable(True)
+      scroll.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+      scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+      main_layout.addWidget(scroll)
+      
 
 class ModelComboBox(QComboBox):
    def __init__(self, parent = None):
@@ -84,8 +102,8 @@ class ModelComboBox(QComboBox):
 
          if dialog.result() == 1 and dialog.textValue():
             self.api_keys[text] = dialog.textValue()
-            print(f'API Key: {dialog.textValue()}')
-            break
+            if self.checkAPIKey(text):
+               break
 
          dialog.setLabelText('Please enter a valid API Key')
 
@@ -94,6 +112,42 @@ class ModelComboBox(QComboBox):
 
    def loadAPI(self, text, api_key):
       print(f'Loading API: {text} with API Key: {api_key[text]}')
+
+   def checkAPIKey(self, text):
+      print(f'Checking API Key: {text}')
+      if text == 'DeepGram':
+         try:
+            # Check if the API key is valid
+            DeepgramClient(api_key=self.api_keys[text])
+         except DeepgramApiKeyError:
+            return False
+         except Exception as e:
+            print('Unexpected error:', e)
+            return False
+         else:
+            return True
+      elif text == 'AssemblyAI':
+         try:
+            aai.settings.api_key = self.api_keys[text]
+            aai.Transcriber()
+         except aai.AssemblyAIError:
+            return False
+         except Exception as e:
+            print('Unexpected error:', e)
+            return False
+         else:
+            return True
+
+class LanguageComboBox(QComboBox):
+   def __init__(self, parent = None):
+      super().__init__(parent)
+      self.addItem('English')
+      self.addItem('Vietnamese')
+      self.addItem('Auto')
+      self.currentTextChanged.connect(self.textChanged)
+
+   def textChanged(self, text):
+      print(f'Language changed to: {text}')
 
 
 if __name__ == "__main__":
